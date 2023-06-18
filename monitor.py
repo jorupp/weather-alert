5f7e781151499ce550c11f6d6c5d4b86f8c25465
Jonathan Rupp <jorupp@gmail.com>
2023-06-18 18:04:36 -0500

    ready to test when API key is activated

--- monitor.py
+++ monitor.py
@@ -14,24 +14,46 @@ ALERT_A = 25  # Temperature threshold A
 ALERT_A_PLUS = ALERT_A + 5  # Temperature threshold A+5
 ALERT_B = 10  # Temperature threshold B
 ALERT_B_MINUS = ALERT_B - 5  # Temperature threshold B-5
+DELAY = 1500 # Wait for 15 minutes (900 seconds)
 
 def get_temperature():
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={LOCATION}&appid={API_KEY}&units=imperial'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=imperial'
     response = requests.get(url)
     data = response.json()
+    print(url)
+    print(data)
     temperature = data['main']['temp']
     return temperature
 
 def get_forecast():
-    url = f'http://api.openweathermap.org/data/2.5/forecast?q={LOCATION}&appid={API_KEY}&units=imperial'
+    url = f'http://api.openweathermap.org/data/2.5/forecast?q={location}&appid={api_key}&units=imperial'
     response = requests.get(url)
     data = response.json()
+    print(data)
     forecast = data['list'][0]['main']['temp']
     return forecast
 
-def send_alert(alert_type):
-    # Replace this with your preferred alert mechanism (e.g., email, SMS)
-    print(f'Alert triggered: Temperature {alert_type}!')
+def play_alert_up():
+    duration = 100
+    frequency = 150
+    winsound.Beep(frequency, duration)
+    frequency = 200
+    winsound.Beep(frequency, duration)
+    frequency = 250
+    winsound.Beep(frequency, duration)
+    frequency = 300
+    winsound.Beep(frequency, duration)
+
+def play_alert_down():
+    duration = 100
+    frequency = 300
+    winsound.Beep(frequency, duration)
+    frequency = 250
+    winsound.Beep(frequency, duration)
+    frequency = 200
+    winsound.Beep(frequency, duration)
+    frequency = 150
+    winsound.Beep(frequency, duration)
 
 def monitor_temperature():
     alert_triggered = {'high': False, 'low': False}
@@ -45,12 +67,12 @@ def monitor_temperature():
             alert_counter['high'] += 1
             if alert_counter['high'] > 2:
                 send_alert('high')
-                alert_triggered['high'] = True
+                play_alert_up()
 
         if temperature < ALERT_B and forecast < ALERT_B_MINUS and not alert_triggered['low']:
             alert_counter['low'] += 1
             if alert_counter['low'] > 2:
-                send_alert('low')
+                play_alert_down()
                 alert_triggered['low'] = True
 
         # Reset alerts counter and trigger status once per day
@@ -58,6 +80,6 @@ def monitor_temperature():
             alert_counter = {'high': 0, 'low': 0}
             alert_triggered = {'high': False, 'low': False}
 
-        time.sleep(900)  # Wait for 15 minutes (900 seconds)
+        time.sleep(DELAY)
 
 monitor_temperature()
